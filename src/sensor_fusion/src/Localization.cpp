8ac3ca8d4ae3cd77ce868563709e1f1746911bc8
Kevin Eppacher <kevin-eppacher@hotmail.de>
2024-05-03 09:38:31 +0000

    Letzten Stand pushen

--- src/sensor_fusion/src/Localization.cpp
+++ src/sensor_fusion/src/Localization.cpp
@@ -8,12 +8,12 @@ Localization::Localization()
     odom_sub = nh.subscribe("odom", 1000, &Localization::odomCallback, this);
 }
 
-Localization::~Localization() 
+Localization::~Localization()
 {
     std::cout << "Lokalisierer wird zerstört" << std::endl;
 }
 
-void Localization::odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+void Localization::odomCallback(const nav_msgs::Odometry::ConstPtr &msg)
 {
     // Process the incoming message
     std::cout << "Received odometry data: " << std::endl;
@@ -22,16 +22,17 @@ void Localization::odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
     std::cout << "Orientation (z): " << msg->pose.pose.orientation.z << std::endl;
 }
 
-
 ParticleFilter::ParticleFilter(int quantityParticles) : quantityParticles(quantityParticles) {}
 
 ParticleFilter::~ParticleFilter() {}
 
-std::vector<Particle> ParticleFilter::initializeParticles(const State& initState) {
+std::vector<Particle> ParticleFilter::initializeParticles(const State &initState)
+{
     std::vector<Particle> particles;
     particles.reserve(quantityParticles);
 
-    for (int i = 0; i < quantityParticles; ++i) {
+    for (int i = 0; i < quantityParticles; ++i)
+    {
         Particle particle;
         particle.x = initState.x;
         particle.y = initState.y;
@@ -44,7 +45,3 @@ std::vector<Particle> ParticleFilter::initializeParticles(const State& initState
 
     return particles;
 }
-
-
-
-
